3d55cba4ff836c5253c90346b293900108a01ac3
Ayush Wardhan (Second Account) <45749740+xshayu@users.noreply.github.com>
2025-03-02 13:32:55 +0000

    feat: Implemented fetching logic for Pinia store to pass tests

--- nuxt-app/stores/posts.ts
+++ nuxt-app/stores/posts.ts
@@ -1,18 +1,25 @@
 import type { Post } from "~/types";
 
 export const usePostStore = defineStore('postStore', () => {
+    const ENDPOINT = 'https://dummyjson.com/posts';
+
     const allPosts = ref({
         posts: [] as Array<Post>,
         total: 0
     });
     
     const loadPosts = async () => {
-        await Promise.resolve();
+        allPosts.value = await $fetch(ENDPOINT);
+        return allPosts.value; // if needed
     };
 
     const fetchPostById = async (id: number) => {
-        await Promise.resolve();
-    }
+        let thisPost = allPosts.value.posts.find(post => post.id == id);
+        if (!thisPost) {
+            thisPost = await $fetch<Post>(`${ENDPOINT}/${id}`);
+        };
+        return thisPost;
+    };
 
     return { allPosts, loadPosts, fetchPostById };
 });
