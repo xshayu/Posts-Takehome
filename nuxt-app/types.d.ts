10387b1ce1d9e2091d2c4f7a14236c64cd882f55
Ayush Wardhan (Second Account) <45749740+xshayu@users.noreply.github.com>
2025-03-02 13:46:58 +0000

    feat: updating Post type based on dummyjson API

--- nuxt-app/types.d.ts
+++ nuxt-app/types.d.ts
@@ -1,3 +1,9 @@
 export interface Post {
     id: number;
+    title: string;
+    body: string;
+    tags: Array<string>;
+    reactions: Record<"likes" | "dislikes", number>;
+    views: number;
+    userId: number;
 };
